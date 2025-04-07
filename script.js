1d98c6608947dd67f53474812148d162f3756dd1
Annalyza106 <160243727+Annalyza106@users.noreply.github.com>
2025-04-07 16:36:59 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector(".sidebar")
+
+function openSidebar () {
+    sidebar.classList.add("show")
+}
+
+function closeSidebar () {
+    sidebar.classList.remove("show")
+}
